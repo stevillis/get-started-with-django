f426d1040146d0e3bc48e68d64b326ca624d1f8d
Stévillis Sousa <stevillis@hotmail.com>
2021-11-05 14:27:40 -0400

    Fix created_on in Post Model and image in Project Model

--- projects/models.py
+++ projects/models.py
@@ -5,4 +5,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='/im')
+    image = models.FilePathField(path='/img')
